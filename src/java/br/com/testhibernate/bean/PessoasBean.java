024de06116a3ab991949a3bf36f1eead93f7316e
everton <everton.nrb@hotmail.com>
2016-11-04 21:35:07 -0300

    Exemplo com hiberte+JPA+PrimeFaces

---
+++ src/java/br/com/testhibernate/bean/PessoasBean.java
@@ -0,0 +1,63 @@
+package br.com.testhibernate.bean;
+
+import br.com.testhibernate.dao.PessoasDao;
+import br.com.testhibernate.entity.Pessoas;
+import java.util.Objects;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class PessoasBean {
+
+    private Pessoas pessoas = new Pessoas();
+    private PessoasDao pessoasDao = new PessoasDao();
+
+    public PessoasBean() {
+    }
+    
+    //metodo para add pessoa
+    public String adicionar(){
+        pessoasDao.addPessoa(pessoas);
+        return "sucesso";
+    }
+    public String remove(){
+        pessoasDao.removePessoa(pessoas);
+        return "sucesso";
+    }
+
+    public Pessoas getPessoas() {
+        return pessoas;
+    }
+
+    public void setPessoas(Pessoas pessoas) {
+        this.pessoas = pessoas;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 71 * hash + Objects.hashCode(this.pessoas);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final PessoasBean other = (PessoasBean) obj;
+        if (!Objects.equals(this.pessoas, other.pessoas)) {
+            return false;
+        }
+        return true;
+    }
+
+    
+}
---
+++ src/java/br/com/testhibernate/dao/PessoasDao.java
@@ -0,0 +1,47 @@
+package br.com.testhibernate.dao;
+
+import br.com.testhibernate.entity.Pessoas;
+import br.com.testhibernate.util.HibernateUtil;
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class PessoasDao {
+
+    private Session sessao;
+    private Transaction trans;
+
+    public void addPessoa(Pessoas p) {
+        try {
+            //Inicaia e abre a sessao
+            sessao = HibernateUtil.getSessionFactory().openSession();
+            //Inicializa
+            trans = sessao.beginTransaction();
+            //Passando uma referencia e carregando os params
+            Pessoas pessoa = new Pessoas();
+            pessoa.setNome(p.getNome());
+            pessoa.setEmail(p.getEmail());
+            pessoa.setTelefone(p.getTelefone());
+            sessao.save(pessoa);
+            trans.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            //finalizando a sessao 
+            sessao.close();
+        }
+
+    }
+    public void removePessoa(Pessoas p){
+        try {
+            sessao = HibernateUtil.getSessionFactory().openSession();
+            trans = sessao.beginTransaction();
+            sessao.delete(p);
+            trans.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            sessao.close();
+        }
+    }
+}
